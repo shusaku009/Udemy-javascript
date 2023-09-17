9139830d4acaa1f472ccb6eabd592d7bf32b7109
shu009 <105494980+shusaku009@users.noreply.github.com>
2023-09-17 04:27:57 +0000

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -50,17 +50,17 @@
 /**
  * テンプレート文字列
  */
-const name = "じゃけぇ";
-const age = 28;
+// const name = "じゃけぇ";
+// const age = 28;
 // 「私の名前はじゃけぇです。年齢は28歳です。」
 
 //　従来の方法
-const message1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
-console.log(message1);
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
+// console.log(message1);
 
 // テンプレート文字列を用いた方法
-const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
-console.log(message2);
+// const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
 
 /**
  * アロー関数
@@ -69,10 +69,10 @@ console.log(message2);
 // function func1(str) {
 //   return str;
 // }
-const func1 = function(str) {
-  return str;
-};
-console.log(func1("func1です"));
+// const func1 = function(str) {
+//   return str;
+// };
+// console.log(func1("func1です"));
 
 // アロー関数
 // const func2 = (str) => {
@@ -81,8 +81,32 @@ console.log(func1("func1です"));
 // console.log(func2("func2です"));
 
 // ↑{} returnを省略した書き方
-const func2 = (str) =>  str;
-console.log(func2("func2です"));
+// const func2 = (str) =>  str;
+// console.log(func2("func2です"));
 
-const func3 = (num1, num2) => num1 + num2;
-console.log(func3(10, 20));
+// const func3 = (num1, num2) => num1 + num2;
+// console.log(func3(10, 20));
+
+/**
+ * 分割代入
+ */
+// const myProfile = {
+//   name: "じゃけぇ",
+//   age: 28,
+// };
+
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}です。`;
+// console.log(message1);
+
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+const myProfile = ['じゃけぇ', 28];
+
+const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+console.log(message3);
+
+const [name, age] = myProfile;
+const message4 = `名前は${name}です。年齢は${age}歳です。`;
+console.log(message4);
